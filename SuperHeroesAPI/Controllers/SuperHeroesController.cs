77a22d350ec7123cfb2f900f52bce42963160571
Knut Anders Aaboe <knutandersaaboe@gmail.com>
2021-06-02 00:32:52 +0200

    Added more information and started with XML documentations

--- SuperHeroesAPI/Controllers/SuperHeroesController.cs
+++ SuperHeroesAPI/Controllers/SuperHeroesController.cs
@@ -20,15 +20,13 @@ namespace SuperHeroesAPI.Controllers
     {
         private readonly ApiFetcher data = new ApiFetcher();
 
-        /// <param name="id">Identity of file to download.</param>
-        /// <returns> file content found.</returns>
+        /// <param name="id">Identity number of the Heroes and Villians.</param>
         [HttpGet("GetAllInfo/{id}")]
         [SwaggerOperation(Summary = "Gets all their information", Description = "Gets all the information there is, on this API, about Heroes/Villians by givving their ID.")]
         [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(AllinfoHV.Root))]
         [SwaggerResponseExample((int)HttpStatusCode.OK, typeof(AllinfoExample))]
         [SwaggerResponse((int)HttpStatusCode.NotFound, Type = typeof(string))]
         [SwaggerResponseExample((int)HttpStatusCode.NotFound, typeof(NotFoundExample))]
-        //[RequireHttps()]
         public ActionResult<AllinfoHV.Root> GetHeroVillianInfo(string id)
         {
             AllinfoHV.Root allInfo = data.GetHeroVillianInfoByID(id);
@@ -38,11 +36,12 @@ namespace SuperHeroesAPI.Controllers
             } else
             {
                 return NotFound(new { error = "Unable to get data with given input/inputs" });
-                //return NotFound();
             }
         }
 
         //Navn på alle superhelt/Villain i apiet - type of ratio to choose all - superhero - villain
+        /// <param name="startID">The identity number you want to start from (max 100 iterations, like 0 - 100)</param>
+        /// <param name="alignment">For both heroes and villians => all. For only Heroes => good. For only Villians => bad</param>
         [HttpGet("GetNames/{startID}&&{alignment}")]
         [SwaggerOperation(Summary = "Gives a list of names", Description = "Gives a list of names of heroes and/or villians. Alignment - good - bad - all (for good and bad) which is default ")]
         [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(String))]
@@ -67,6 +66,8 @@ namespace SuperHeroesAPI.Controllers
 
 
         //Implementer pvp apiet (ordne med navn istedenfor id?)
+        /// <param name="id">The identity number of the Hero or Villian.</param>
+        /// <param name="id2">The identity number of the second Hero ir Villian.</param>
         [HttpGet("Comparisons/{id}&&{id2}")]
         [SwaggerOperation(Summary = "Comparing two hero/villian given id", Description = "Comparing two hero/villian given their id, this includes powerstats")]
         [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<Comparisons.Data>))]
@@ -87,6 +88,8 @@ namespace SuperHeroesAPI.Controllers
             }
         }
 
+        /// <param name="letters">The letters starting/containing your hero/villian.</param>
+        /// <param name="where">Where your letters are, starts or contains.</param>
         [HttpGet("StartsORcontains/{letters}&&{where}")]
         [SwaggerOperation(Summary = "Gives heroes/villians name and id maching given letters", Description = "Gives heroes/villians name and id maching given letter/letters. Where - starts - contains, contains is default")]
         [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(Search.Results))]
--- SuperHeroesAPI/Startup.cs
+++ SuperHeroesAPI/Startup.cs
@@ -10,7 +10,10 @@ using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -35,13 +38,36 @@ namespace SuperHeroesAPI
                 //To be able to show response examples
                 c.ExampleFilters();
                 //To be able to use SwashBuckle Annotations
-                //c.EnableAnnotations();
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HeroesVilliansAPI", Version = "v1" });
-            });
-            //GlobalConfiguration.Configuration.EnableSwagger(c =>
-            //    {
+                c.EnableAnnotations();
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                { 
+                    Version = "v1",
+                    Title = "HeroesAndVilliansAPI",
+                    Description = "A simple ASP.NET Core Web API using SuperHeroAPI",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    //While figuring stuff out
+                    Contact = new OpenApiContact
+                    {
+                        Name = "SuperHeroAPI",
+                        Email = string.Empty,
+                        Url = new Uri("https://superheroapi.com/")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
 
-            //});
+                //For XML documentations, Reflection used to build XML file maching our API project. 
+                // BaseDirectory to construct a oath to the XML file
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+            
+
+            //Allows for resonse examples
             services.AddSwaggerExamplesFromAssemblyOf<Startup>();
 
         }
@@ -52,11 +78,11 @@ namespace SuperHeroesAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
+                app.UseSwagger(c => c.SerializeAsV2 = true);
                 app.UseSwaggerUI(c =>
                 {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "SuperHeroesAPI v1");
-                    //c.RoutePrefix = string.Empty;
+                    c.RoutePrefix = "swagger";
+                    c.SwaggerEndpoint("v1/swagger.json", "SuperHeroesAPI v1"); // /swagger/v1/swagger.json", "SuperHeroesAPI v1      v1/swagger.json", "SuperHeroesAPI v1
                 });
             }
 
